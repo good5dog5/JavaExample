b73f69958bb9246e08465974d68b36f6a129545e
Shark0 <lin22223344@gmail.com>
2019-09-24 10:03:17 +0800

    新增 "寄送Email Example" 新增 "Excel Example"

---
+++ src/main/java/com/shark/example/SendEmailExample.java
@@ -0,0 +1,49 @@
+package com.shark.example;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class SendEmailExample {
+    public static void main(String argv[]) {
+        final String from = "from";
+        final String password = "password";
+        final String to = "to";
+        Properties properties = System.getProperties();
+        properties.put("mail.mime.charset","UTF-8");
+        properties.setProperty("mail.transport.protocol", "smtp");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.port", "587");
+        properties.put("mail.smtp.auth", "true");
+        Session session = Session.getInstance(properties, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(from,password);
+            }
+        } );
+        Transport transport = null;
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(from));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+            message.setSubject("標題測試", "UTF-8");
+            message.setText("內容測試", "UTF-8");
+            transport = session.getTransport();
+            transport.connect();
+            transport.send(message);
+            transport.close();
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        } finally {
+            if (transport != null) {
+                try {
+                    transport.close();
+                } catch (MessagingException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
