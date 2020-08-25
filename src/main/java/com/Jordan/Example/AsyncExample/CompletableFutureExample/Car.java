197492ebd36ba67c2d83cd314ee29fa75e9f7b7f
jordan <jordan.huang@gapp.nthu.edu.tw>
2020-08-25 11:40:36 +0800

    Add async.

--- src/main/java/com/Jordan/Example/AsyncExample/CompletableFutureExample/Car.java
+++ src/main/java/com/Jordan/Example/AsyncExample/CompletableFutureExample/Car.java
@@ -1,4 +1,22 @@
 package com.Jordan.Example.AsyncExample.CompletableFutureExample;
 
-public class Car {
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+class Car {
+    int id;
+    int manufacturerId;
+    String model;
+    int year;
+    float rating;
+
+    void setRating(float rating) {
+        this.rating = rating;
+    }
+
+    @Override
+    public String toString() {
+        return "Car (id=" + id + ", manufacturerId=" + manufacturerId + ", model=" + model + ", year=" + year
+                + ", rating=" + rating;
+    }
 }
