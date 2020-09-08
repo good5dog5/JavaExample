b944de6b1de8f6f2739ce2d360dcce1aa0e403af
jordan <jordan.huang@gapp.nthu.edu.tw>
2020-09-08 11:25:49 +0800

    New enum use case.

---
+++ src/main/java/com/EffectiveJava/Chapter6/item35/Ensemble.java
@@ -0,0 +1,17 @@
+package com.EffectiveJava.Chapter6.item35;
+
+// enum with int data stored in an instance field (p.168)
+public enum Ensemble {
+    SOLO(1), DUET(2), TRIO(3), QUARTET(4), QUINTET(5),
+    SEXTET(6), SEPTET(7), OCTET(8), DOUBLE_QUARTET(8),
+    NONET(9), DECTET(10), TRIPLE_QUARTET(12);
+
+
+    private final int numberOfMusicians;
+    Ensemble(int size) { this.numberOfMusicians = size ;}
+
+    public int getNumberOfMusicians() { return numberOfMusicians;}
+
+
+
+}
