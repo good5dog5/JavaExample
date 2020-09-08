15be75cee651da41a0f861743c8c792b7cb6e6ed
jordan <jordan.huang@gapp.nthu.edu.tw>
2020-09-08 10:17:42 +0800

    test send mail.

---
+++ src/main/java/com/Jordan/Example/SendEmailExample2.java
@@ -0,0 +1,59 @@
+package com.Jordan.Example;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class SendEmailExample2 {
+    public static void main(String argv[]) {
+        TimeUnit unit;
+        BlockingQueue workQueue;
+        final String from = "from";
+        final String password = "password";
+        final String to = "to";
+        Properties properties = System.getProperties();
+        properties.put("mail.mime.charset","UTF-8");
+        properties.setProperty("mail.transport.protocol", "smtp");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.port", "587");
+        properties.put("mail.smtp.auth", "true");
+        Session session = Session.getInstance(properties, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(from,password);
+            }
+        } );
+        Transport transport = null;
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(from));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+            message.setSubject("標題測試", "UTF-8");
+            message.setText("內容測試", "UTF-8");
+            transport = session.getTransport();
+            transport.connect();
+            transport.send(message);
+            transport.close();
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        } finally {
+            if (transport != null) {
+                try {
+                    transport.close();
+                } catch (MessagingException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
