0ae4849da15a5ada89e8f9bbba5b913e0b9fbca9
jordan <jordan.huang@gapp.nthu.edu.tw>
2020-09-25 17:22:53 +0800

    update.

--- src/main/java/com/Jordan/Example/FileSystemRelated/PathExample.java
+++ src/main/java/com/Jordan/Example/FileSystemRelated/PathExample.java
@@ -10,33 +10,39 @@ import java.util.stream.Stream;
 
 public class PathExample {
     public static void main(String[] args) throws IOException {
+        System.out.println(pathJoinExample());
 
-        Path currentWorkingPath = Paths.get("").toAbsolutePath();
-        Path path = Paths.get(currentWorkingPath.toString() + "/file/"  + "band.txt");
-
-        String currentWorkingDirectory = new File(".").getAbsolutePath();
-        File bands_txt = new File(currentWorkingDirectory + "/file/" + "bands.txt");
-
-
-        // file to path
-        Path convertedPath =  bands_txt.toPath();
-        File converedFile = path.toFile();
-
-        System.out.println("abs path: " + path.toString());
-        System.out.println("getFileName(): " + path.getFileName());
-        System.out.println("getName(0): " + path.getName(0));
-        System.out.println("getNameCount(): " + path.getNameCount());
-        System.out.println("subPath(0, 2): " + path.subpath(0, 2));
-        System.out.println("getParent(): " + path.getParent());
-        System.out.println("getRoot(): " + path.getRoot());
-
-        Stream<Path> javaPaths = generatePaths();
-        javaPaths.forEach(System.out::println);
+//        Path currentWorkingPath = Paths.get("").toAbsolutePath();
+//        Path path = Paths.get(currentWorkingPath.toString() + "/file/"  + "band.txt");
+//
+//        String currentWorkingDirectory = new File(".").getAbsolutePath();
+//        File bands_txt = new File(currentWorkingDirectory + "/file/" + "bands.txt");
+//
+//
+//        // file to path
+//        Path convertedPath =  bands_txt.toPath();
+//        File converedFile = path.toFile();
+//
+//        System.out.println("abs path: " + path.toString());
+//        System.out.println("getFileName(): " + path.getFileName());
+//        System.out.println("getName(0): " + path.getName(0));
+//        System.out.println("getNameCount(): " + path.getNameCount());
+//        System.out.println("subPath(0, 2): " + path.subpath(0, 2));
+//        System.out.println("getParent(): " + path.getParent());
+//        System.out.println("getRoot(): " + path.getRoot());
+//
+//        Stream<Path> javaPaths = generatePaths();
+//        javaPaths.forEach(System.out::println);
     }
 
     private static Stream<Path> generatePaths() throws IOException {
         return Files.walk(Paths.get(""))
                 .filter(p -> p.getFileName().endsWith("java"));
 
+    }
+
+    private static Path pathJoinExample() {
+        Path path = Paths.get("").toAbsolutePath();
+        return Paths.get(path.toString(), "foo", "bar", "baz.txt");
     }
 }
