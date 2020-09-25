8ba240f33d58183179c07f7ee635907c979e2a53
jordan <jordan.huang@gapp.nthu.edu.tw>
2020-09-25 12:04:11 +0800

    updates.

--- src/main/java/com/Jordan/Example/stream/IntStreamExample.java
+++ src/main/java/com/Jordan/Example/stream/IntStreamExample.java
@@ -5,11 +5,13 @@ import java.util.stream.IntStream;
 
 public class IntStreamExample {
     public static void main(String[] args) {
-        int [] arr = new int[5];
-        Arrays.fill(arr, 10);
 
         // use IntStream
-        int [] arr1 = IntStream.rangeClosed(1, 5).map(s->10).toArray();
-        System.out.println(Arrays.toString(arr1));
+        int [] arr = IntStream
+                .rangeClosed(0, 10)
+                .map(s-> s*2).toArray();
+
+
+        System.out.println(Arrays.toString(arr));
     }
 }
